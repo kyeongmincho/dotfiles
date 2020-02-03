e93bacfe56e922f77a058e0f0fe7a9a0e171b261
Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>
2020-02-04 03:51:17 +0900

    vscode: add extension list
    Signed-off-by: Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>

---
+++ vscode/install_ext.sh
@@ -0,0 +1,12 @@
+# From this command:
+# code --list-extensions | xargs -L 1 echo code --install-extension
+
+code --install-extension dhedgecock.radical-vscode
+code --install-extension eamodio.gitlens
+code --install-extension maximedenes.vscoq
+code --install-extension mhutchie.git-graph
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension siegebell.prettify-symbols-mode
+code --install-extension siegebell.proof-general-keybindings-for-vscoq
+code --install-extension vscode-icons-team.vscode-icons
