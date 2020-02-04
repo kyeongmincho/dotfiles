5fe4bb058ee3de253f8bba90c9ff1d2c8d8cd0f0
Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>
2020-02-05 00:02:44 +0900

    vscode: add Rust extensions pack
    Signed-off-by: Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>

--- vscode/install_ext.sh
+++ vscode/install_ext.sh
@@ -1,12 +1,15 @@
 # From this command:
 # code --list-extensions | xargs -L 1 echo code --install-extension
 
+code --install-extension bungcip.better-toml
 code --install-extension dhedgecock.radical-vscode
 code --install-extension eamodio.gitlens
+code --install-extension matklad.rust-analyzer
 code --install-extension maximedenes.vscoq
 code --install-extension mhutchie.git-graph
 code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
+code --install-extension serayuzgur.crates
 code --install-extension siegebell.prettify-symbols-mode
 code --install-extension siegebell.proof-general-keybindings-for-vscoq
 code --install-extension vscode-icons-team.vscode-icons
