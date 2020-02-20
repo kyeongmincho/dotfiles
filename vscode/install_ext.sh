fe50ddddfa61fc43cfce9d368cebae9befb5439c
Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>
2020-02-20 18:11:10 +0900

    vscode: add rust test explorer ext
    Signed-off-by: Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>

--- vscode/install_ext.sh
+++ vscode/install_ext.sh
@@ -6,6 +6,7 @@ code --install-extension CoenraadS.bracket-pair-colorizer-2
 code --install-extension dunstontc.vscode-rust-syntax
 code --install-extension eamodio.gitlens
 code --install-extension Gruntfuggly.todo-tree
+code --install-extension hbenl.vscode-test-explorer
 code --install-extension matklad.rust-analyzer
 code --install-extension maximedenes.vscoq
 code --install-extension mhutchie.git-graph
@@ -15,5 +16,6 @@ code --install-extension rust-lang.rust
 code --install-extension serayuzgur.crates
 code --install-extension siegebell.prettify-symbols-mode
 code --install-extension siegebell.proof-general-keybindings-for-vscoq
+code --install-extension Swellaby.vscode-rust-test-adapter
 code --install-extension vadimcn.vscode-lldb
 code --install-extension vscode-icons-team.vscode-icons
