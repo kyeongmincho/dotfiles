37f3eecd2080291622f5702b58f9fcc78d6e2a68
Kyeongmin Cho <Kyeongmin.cho@kaist.ac.kr>
2020-03-30 07:23:10 -0700

    zsh: fix error
    Signed-off-by: Kyeongmin Cho <Kyeongmin.cho@kaist.ac.kr>

--- zsh/install_plugin.sh
+++ zsh/install_plugin.sh
@@ -1,11 +1,26 @@
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# power level 10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # autojump
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    sudo apt install autojump
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install autojump
+case "$(uname -s)" in
+   Darwin)
+     brew install autojump
+     ;;
+
+   Linux)
+     sudo apt install autojump
+     ;;
+
+    *)
+     echo 'Other OS'
+     ;;
+esac
+
+cp ./.zshrc $HOME/.zshrc
+cp ./.p10k.zsh $HOME/.p10k.zsh
